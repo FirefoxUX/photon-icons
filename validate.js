d18478ce08bbbd203fdfc216bf578e89a33e7b8a
Blake Winton <bwinton@latte.ca>
2018-06-05 11:31:43 -0400

    Make sure everything has a correct viewBox.

--- validate.js
+++ validate.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
-var shell = require('shelljs');
+const shell = require('shelljs');
+const fs = require('fs');
+const {icons} = require('./photon-icons.json');
+
+const SIZE_RE = /<svg xmlns="http:\/\/www.w3.org\/2000\/svg" width="(\d+)" height="(\d+)" viewBox="0 0 (\d+) (\d+)"/;
+
 const images = shell.ls('-R', 'icons')
   .map(image => `icons/${image}`)
   .filter(image => image.includes('.'));
-const unseen = images.slice();
-const {icons} = require('./photon-icons.json');
-const missing = [];
-
 let prevname = "";
+
+const missing = [];
+const unseen = images.slice();
 const out_of_order = [];
+const bad_sizes = [];
 
 for (let icon of icons) {
   let currname = icon.categories.join(':') + '/';
@@ -27,11 +32,23 @@ for (let icon of icons) {
       let image = icon.source[source][size];
       let exists = images.includes(image);
       let unseenIndex = unseen.indexOf(image);
-      if (!exists) {
-        missing.push(`${image} (from ${icon.name}.${source}.${size})`)
-      }
       if (unseenIndex != -1) {
         unseen.splice(unseenIndex, 1);
+      }
+      if (!exists) {
+        missing.push(`${image} (from ${icon.name}.${source}.${size})`)
+      } else {
+        // check for the correct viewbox.
+        let data = fs.readFileSync(image, "utf-8");
+        let matches = SIZE_RE.exec(data);
+        if (!matches) {
+          bad_sizes.push(`${image} does not have a proper viewBox.`);
+        } else {
+          let sizes = matches.slice(1);
+          if (!sizes.every(size => size == sizes[0])) {
+            bad_sizes.push(`${image} isn't square with a proper viewbox: [${sizes}]`);
+          }
+        }
       }
     }
   }
@@ -43,7 +60,9 @@ if (missing.length) {
 if (unseen.length) {
   console.log(`Extra files:\n  ${unseen.join("\n  ")}`);
 }
-
 if (out_of_order.length) {
   console.log(`Out of order entries:\n  ${out_of_order.join("\n  ")}`);
+}
+if (bad_sizes.length) {
+  console.log(`Poorly-sized entries:\n  ${bad_sizes.join("\n  ")}`);
 }
