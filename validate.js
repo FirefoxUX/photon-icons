8228c5089a38576f340dc670ffb3683174599893
Blake Winton <bwinton@latte.ca>
2018-06-27 16:03:06 -0400

    Fix the validator to be more lenient, and give better error messages.

--- validate.js
+++ validate.js
@@ -5,7 +5,8 @@ const shell = require('shelljs');
 const fs = require('fs');
 const {icons} = require('./photon-icons.json');
 
-const SIZE_RE = /<svg xmlns="http:\/\/www.w3.org\/2000\/svg" width="(\d+)" height="(\d+)" viewBox="0 0 (\d+) (\d+)"/;
+const SVG_RE = /<svg [^>]*>/;
+const interesting_attrs = ['xmlns', 'width', 'height', 'viewBox'];
 
 const images = shell.ls('-R', 'icons')
   .map(image => `icons/${image}`)
@@ -29,7 +30,7 @@ for (let icon of icons) {
   }
   prevname = currname;
   for (let source in icon.source) {
-    for (let size in icon.source[source]) {
+    nextImage: for (let size in icon.source[source]) {
       let image = icon.source[source][size];
       let exists = images.includes(image);
       let unseenIndex = unseen.indexOf(image);
@@ -38,18 +39,43 @@ for (let icon of icons) {
       }
       if (!exists) {
         missing.push(`${image} (from ${icon.name}.${source}.${size})`)
-      } else {
-        // check for the correct viewbox.
-        let data = fs.readFileSync(image, "utf-8");
-        let matches = SIZE_RE.exec(data);
-        if (!matches) {
-          bad_sizes.push(`${image} does not have a proper viewBox.`);
-        } else {
-          let sizes = matches.slice(1);
-          if (!sizes.every(size => size == sizes[0])) {
-            bad_sizes.push(`${image} isn't square with a proper viewbox: [${sizes}]`);
-          }
+        continue nextImage;
+      }
+
+      // check for the correct viewbox.
+      let data = fs.readFileSync(image, "utf-8");
+      let matches = SVG_RE.exec(data);
+      let ATTR_RE = /[a-zA-Z0-9]+="[^"]*"/g;
+      let attr = null;
+      let attrs = {};
+      while ((attr = ATTR_RE.exec(matches[0])) != null) {
+        let [key, val] = attr[0].split("=");
+        if (interesting_attrs.indexOf(key) == -1) {
+          continue;
         }
+        // Make sure we don't have duplicate attributes.
+        if (attrs[key] != undefined) {
+          bad_sizes.push(`${image} has more than one ${key}.`);
+          continue nextImage;
+        }
+        attrs[key] = val.slice(1, val.length - 1);
+      }
+
+      // Make sure all the things we're interested in are there.
+      for (let key of interesting_attrs) {
+        if (!attrs[key]) {
+          bad_sizes.push(`${image} is missing ${key}.`);
+          continue nextImage;
+        }
+      }
+      let attr_size = attrs['width'];
+      if (attrs['height'] != attr_size) {
+        bad_sizes.push(`${image} has a height different than "${attr_size}".`);
+        continue nextImage;
+      }
+      if (attrs['viewBox'] != `0 0 ${attr_size} ${attr_size}`) {
+        bad_sizes.push(`${image} has a viewbox different than "0 0 ${attr_size} ${attr_size}".`);
+        continue nextImage;
       }
     }
   }
